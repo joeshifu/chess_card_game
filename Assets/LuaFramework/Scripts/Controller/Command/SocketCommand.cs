d921fc0db7679df9ce2766fc4dd81e7fc26b3e35
iamjoeshifu <512614226@qq.com>
2016-11-03 11:42:17 +0800

    add

---
+++ Assets/LuaFramework/Scripts/Controller/Command/SocketCommand.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LuaFramework;
+
+public class SocketCommand : ControllerCommand
+{
+
+    public override void Execute(IMessage message)
+    {
+        object data = message.Body;
+        if (data == null) return;
+        KeyValuePair<int, ByteBuffer> buffer = (KeyValuePair<int, ByteBuffer>)data;
+        switch (buffer.Key)
+		{
+			//把packet.header.header，packet.data 抛到lua里面去处理
+            default: Util.CallMethod("Network", "OnSocket", buffer.Key, buffer.Value); break;
+        }
+    }
+}
